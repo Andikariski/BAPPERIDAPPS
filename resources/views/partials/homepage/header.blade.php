1ae9c11e23ee3f54ff5679e03b54521509cdf70b
Andika Riski <andikariski207@gmail.com>
2025-09-02 22:24:58 +0900

    Partials seccion

---
+++ resources/views/partials/homepage/header.blade.php
@@ -0,0 +1,58 @@
+<header id="header" class="header d-flex align-items-center sticky-top">
+      <div
+        class="container-fluid container-xl position-relative d-flex align-items-center">
+        <a href="index.html" class="logo d-flex align-items-center me-auto">
+          <!-- Uncomment the line below if you also wish to use an image logo -->
+          <img src="assets/img/pps.png" alt="" />
+          <h4 class="sitename">BAPPERIDA PPS</h4>
+        </a>
+
+        <nav id="navmenu" class="navmenu">
+          <ul>
+            <li>
+              <a href="#hero" class="active">Beranda<br /></a>
+            </li>
+            <li class="dropdown">
+              <a href="#"
+                ><span>Tentang</span>
+                <i class="bi bi-chevron-down toggle-dropdown"></i
+              ></a>
+              <ul>
+                <li><a href="#">Pengantar Kepala Badan</a></li>
+                <li><a href="#">Profile Pegawai</a></li>
+                <li><a href="#">Struktur Organisasi</a></li>
+                <li><a href="#">Tugas Pokok dan Fungsi</a></li>
+              </ul>
+            </li>
+            <li class="dropdown">
+              <a href="#"
+                ><span>Berita</span>
+                <i class="bi bi-chevron-down toggle-dropdown"></i
+              ></a>
+              <ul>
+                <li><a href="#">Berita dan Informasi</a></li>
+                <li><a href="#">Galeri Foto</a></li>
+              </ul>
+            </li>
+            <li class="dropdown">
+              <a href="#"
+                ><span>Dokumen Publik</span>
+                <i class="bi bi-chevron-down toggle-dropdown"></i
+              ></a>
+              <ul>
+                <li><a href="#">RKPD</a></li>
+                <li><a href="#">RTRW</a></li>
+                <li><a href="#">RPJMD</a></li>
+                <li><a href="#">RPJPD</a></li>
+                <li><a href="#">LKPJ</a></li>
+                <li><a href="#">Dokumen Lain</a></li>
+              </ul>
+            </li>
+            <li><a href="#about">Informasi Lainya</a></li>
+          </ul>
+          <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
+        </nav>
+
+        <a class="btn-getstarted" href="#about">Mulai</a>
+      </div>
+    </header>
