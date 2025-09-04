fdb5de37e33cb8488cac0cab0dc4a2bd068581b4
Andika Riski <andikariski207@gmail.com>
2025-09-04 15:46:56 +0900

    New testing

--- resources/views/partials/homepage/header.blade.php
+++ resources/views/partials/homepage/header.blade.php
@@ -53,6 +53,7 @@
           <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
         </nav>
 
-        <a class="btn-getstarted" href="#about">Mulai</a>
+        <a class="btn-getstarted" href="{{ route('login') }}">Masuk</a>
+        {{-- <a href="{{ route('login') }}" class="inline-block px-5 py-1.5 dark:text-[#EDEDEC] text-[#1b1b18] border border-transparent hover:border-[#19140035] dark:hover:border-[#3E3E3A] rounded-sm text-sm leading-normal"> Log in</a> --}}
       </div>
     </header>
