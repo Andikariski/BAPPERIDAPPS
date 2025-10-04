d4202208f8fefa144b15348f7a2fd783ef02b6d6
PAPATIRI <syariftaufik11@gmail.com>
2025-10-04 09:56:09 +0700

    fix(bugs): fix bug di searching dan filtering berita karena mengembalikan 2 elemen yaitu tag div dan tag style

--- resources/views/livewire/blog/index.blade.php
+++ resources/views/livewire/blog/index.blade.php
@@ -1,25 +1,17 @@
 @section('title', 'Admin | Berita')
-<style>
-.line-clamp-2 {
-  display: -webkit-box;
-  -webkit-line-clamp: 2;  /* jumlah baris yang tampil */
-  -webkit-box-orient: vertical;
-  overflow: hidden;
-  text-overflow: ellipsis;
-}
-</style>
 <div class="container py-2">
-     <div class="container section-title mt-5" data-aos="fade-up">
-            <h2>Berita</h2>
-            <p>Segala Berita Tentang Kegiatan dan Aktivitas BAPPERIDA PPS</p>
+    <div class="container section-title mt-5" data-aos="fade-up">
+        <h2>Berita</h2>
+        <p>Segala Berita Tentang Kegiatan dan Aktivitas BAPPERIDA PPS</p>
     </div>
 
     <!-- Search & filtering -->
     <div class="d-flex justify-content-between gap-2">
-        <input type="text" class="form-control mb-3" placeholder="cari berita..." wire:model.live="search">
+        <input type="text" class="form-control mb-3" placeholder="cari berita..."
+            wire:model.live.debounce.500ms="search">
         <div class="mb-3">
-            <select wire:model.live="bidang" class="form-select">
-                <option class="px-4" value="" selected>Pilih Bidang</option>
+            <select wire:model.live="selectedBidang" class="form-select">
+                <option class="px-4" value="">Pilih Bidang</option>
                 @foreach ($dataBidang as $bidang)
                     <option value="{{ $bidang->id }}">{{ $bidang->nama_bidang }}</option>
                 @endforeach
@@ -42,18 +34,23 @@
                         </div>
                     @endif
                     <div class="card-body">
-                        <h5 class="card-title text-bold"><strong>{{ $post->judul_berita }}</strong></h5>
-                        <p class="line-clamp-2">{{ $post->konten_berita }}</p>
-                        <span class="text-muted" style="font-size: 0.90rem;">{{  $post->created_at->diffForHumans() }} | Oleh{{ $post->author->name }}</span>
-                        {{-- <p class="card-text">{{ $post->created_at->translatedFormat('j F Y') }}</p> --}}
+                        <div class="mb-3">
+                            <h5 class="card-title text-bold"><strong>{{ $post->judul_berita }}</strong></h5>
+                            <p>{{ Str::limit(strip_tags($post->konten_berita), 100) }}</p>
+                            <span class="text-muted"
+                                style="font-size: 0.90rem;">{{ $post->created_at->diffForHumans() }} |
+                                Oleh {{ $post->author->name }}</span>
+                        </div>
 
-                        <a href="{{ route('blog.show', $post->slug) }}" class="badge text-bg-primary">Selengkapnya</a>
+                        <div class="text-end">
+                            <a href="{{ route('blog.show', $post->slug) }}" class="text-primary">Selengkapnya</a>
+                        </div>
                     </div>
                 </div>
             </div>
         @empty
             <div class="alert alert-warning d-flex align-items-center justify-content-center">
-                <p class="my-5">No posts found.</p>
+                <p class="my-5">tidak ada berita ditemukan</p>
             </div>
         @endforelse
     </div>
@@ -61,5 +58,4 @@
     <div class="mt-4">
         {{ $posts->links('vendor.livewire.bootstrap-pagination') }}
     </div>
-
 </div>
